1f0ae23c779d62ba59b8af27c0afbf1f63d60e09
hotequil <hotequil@protonmail.com>
2022-08-15 20:45:34 -0300

    feat: init person entity

---
+++ src/entities/person.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, full_name, birth_date, phone, email):
+        self._full_name = full_name
+        self._birth_date = birth_date
+        self._phone = phone
+        self._email = email
