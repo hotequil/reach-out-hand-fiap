c112c5957b797ccf56b2d111811d412cbc389e42
hotequil <hotequil@protonmail.com>
2022-08-19 20:50:14 -0300

    feat: done person factory

--- src/factories/person.py
+++ src/factories/person.py
@@ -1,18 +1,19 @@
-# from utils.print_helper import question, print_title_both, print_bar_bottom, print_bar_top
-# from entities.person import Person
-#
-# def create_person():
-#     print_bar_bottom()
-#     print_title_both("# Cadastro de nova pessoa")
-#
-#     person = Person(
-#         question("- Nome completo: "),
-#         question("- Data de nascimento: "),
-#         question("- Telefone: "),
-#         question("- Email: ")
-#     )
-#
-#     person.add_categories()
-#
-#     print_title_both("Pessoa cadastrada com sucesso!")
-#     print_bar_top()
+from utils.print_helper import question, print_title_both, print_bar_bottom, print_bar_top
+from entities.person import Person
+
+def create_person():
+    print_bar_bottom()
+    print_title_both("# Cadastro de nova pessoa")
+
+    person = Person(
+        question("- Nome completo: "),
+        question("- Data de nascimento: "),
+        question("- Telefone: "),
+        question("- Email: ")
+    )
+
+    person.add_categories()
+    person.add_data()
+
+    print_title_both("Pessoa cadastrada com sucesso!")
+    print_bar_top()
