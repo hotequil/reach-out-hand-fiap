0d3f25e0cbddd33665a8bc471f500558fd91b6ec
hotequil <hotequil@protonmail.com>
2022-08-18 22:19:22 -0300

    feat: done categories helper with new methods

--- src/utils/categories_helper.py
+++ src/utils/categories_helper.py
@@ -1,6 +1,14 @@
 from enums.category_number import CategoryNumber
+from enums.category_type import CategoryType
 
 category_numbers_list = [category_number.value for category_number in CategoryNumber]
+conflicted_category_error = f"{CategoryType.EMPLOYEE.value} ou {CategoryType.DONOR.value.lower()} não pode estar junto com a categoria {CategoryType.SERVED.value.lower()}"
 
 def is_valid_category_number(category_number):
     return category_number in category_numbers_list
+
+def get_invalid_categories(categories):
+    return list(map(str, [category_number for category_number in categories if not is_valid_category_number(category_number)]))
+
+def has_conflicted_category(categories):
+    return (CategoryNumber.SERVED.value in categories) and ((CategoryNumber.EMPLOYEE.value in categories) or (CategoryNumber.DONOR.value in categories))
