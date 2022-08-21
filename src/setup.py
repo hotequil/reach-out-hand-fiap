4f992e50c6adfb80b3a14376860f573f3b61cdef
hotequil <hotequil@protonmail.com>
2022-08-20 22:12:22 -0300

    feat: added search person option in setup

--- src/setup.py
+++ src/setup.py
@@ -13,13 +13,14 @@ def init():
     while not stop:
         print_title_top("# Menu")
 
-        option = input(f"Escolha uma das seguintes opções abaixo: {double_break_line}{SetupOption.STOP_SYSTEM.value}) Sair do sistema; {break_line}{SetupOption.ADD_PERSON.value}) Cadastrar pessoa; {break_line}{SetupOption.LIST_PEOPLE.value}) Listar pessoas ordenadas por nome e categorias; {double_break_line}Resposta: ")
+        option = input(f"Escolha uma das seguintes opções abaixo: {double_break_line}{SetupOption.STOP_SYSTEM.value}) Sair do sistema; {break_line}{SetupOption.ADD_PERSON.value}) Cadastrar pessoa; {break_line}{SetupOption.LIST_PEOPLE.value}) Listar pessoas ordenadas por nome e categorias; {break_line}{SetupOption.SEARCH_PERSON.value}) Buscar pessoa por nome; {double_break_line}Resposta: ")
 
         if option.isnumeric(): option = int(option)
 
         if option == SetupOption.STOP_SYSTEM.value: stop = True
         elif option == SetupOption.ADD_PERSON.value: people.add(create_person())
         elif option == SetupOption.LIST_PEOPLE.value: people.list()
+        elif option == SetupOption.SEARCH_PERSON.value: people.search()
         else:
             print_space()
             normal_error("Você digitou uma opção inválida, tente novamente!")
